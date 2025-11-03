48c2e5fcdbfe31caa638c6a0d678a0b4d771ae4d
Sivaranjani <msivaranjani2036@gmail.com>
2025-11-03 17:23:36 +0530

    admin sider

--- app/admin/components/AdminSider.js
+++ app/admin/components/AdminSider.js
@@ -26,19 +26,19 @@ export default function AdminSider({ collapsed }) {
         { icon: 'mdi:filter-outline', label: 'Filter', link: 'filter', dotColor: 'bg-yellow-500' }
       ]
     },
-    { icon: 'mdi:image-outline', label: 'Banner', link: 'design' },
+    // { icon: 'mdi:image-outline', label: 'Banner', link: 'design' },
     {
-      icon: 'material-symbols:receipt-long',
-          label: 'Sales',
-    submenu: [
-      { icon: 'mdi:clock-outline', label: 'All Orders', link: 'Allorder', dotColor: 'bg-yellow-500' },
-      { icon: 'mdi:clock-outline', label: 'Home Delivery', link: 'homedelivery', dotColor: 'bg-yellow-500' },
-      // { icon: 'mdi:clock-outline', label: 'Home Delivery', link: 'order/home-delivery', dotColor: 'bg-yellow-500' },
-      // { icon: 'mdi:clock-outline', label: 'Pending Order', link: 'order/pending-order', dotColor: 'bg-yellow-500' },
-      { icon: 'mdi:cancel', label: 'Cancel Order', link: 'order/cancel-order', dotColor: 'bg-red-500' },
-      { icon: 'mdi:truck-delivery-outline', label: 'Shipped Order', link: 'shippedorder', dotColor: 'bg-green-500' }
-    ]
-  },
+        icon: 'material-symbols:receipt-long',
+            label: 'Sales',
+      submenu: [
+        { icon: 'mdi:clock-outline', label: 'All Orders', link: 'Allorder', dotColor: 'bg-yellow-500' },
+        { icon: 'mdi:clock-outline', label: 'Home Delivery', link: 'homedelivery', dotColor: 'bg-yellow-500' },
+        // { icon: 'mdi:clock-outline', label: 'Home Delivery', link: 'order/home-delivery', dotColor: 'bg-yellow-500' },
+        // { icon: 'mdi:clock-outline', label: 'Pending Order', link: 'order/pending-order', dotColor: 'bg-yellow-500' },
+        { icon: 'mdi:cancel', label: 'Cancel Order', link: 'order/cancel-order', dotColor: 'bg-red-500' },
+        { icon: 'mdi:truck-delivery-outline', label: 'Shipped Order', link: 'shippedorder', dotColor: 'bg-green-500' }
+      ]
+    },
     { icon: 'mdi:tag-outline', label: 'Offer', link: 'offer' },
     { icon: 'mdi:note-text-outline', label: 'Blog', link: 'blog' },
     { icon: 'mdi:account-outline', label: 'User', link: 'user' },
